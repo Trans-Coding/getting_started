aeed57020e7b673272c50d9dfde605601c35e90e
Trans-Coding <47959118+Trans-Coding@users.noreply.github.com>
2019-03-10 22:23:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<html>
-	<head>
-		<title>My homepage</title>
-	</head>
-	<body>
-		<h1>Web site tutorials</h1>
-		<br/>
-		<h2>Never more than 1 h1 tag</h2>
-
-		<p>this is a <strong>paragraph</strong></p>
-		<hr/>
-		<!-- this is a note -->
-		<p>this is another <em>paragraph</em></p>
-	</body>
-</html>
+<?php phpinfo(); ?>
