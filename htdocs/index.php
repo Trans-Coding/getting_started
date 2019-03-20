66e5d01568cdb3f3a521b5bd5b2d6732bc22d07b
Jade <jade.transcoding@gmail.com>
2019-03-19 21:10:46 -0700

    In depth HTML presentation
    build out of resume and teaching of in depth HTML via it's creations

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,15 +1,23 @@
-<html>
+<!DOCTYPE html>
+
+<html lang="en">
 	<head>
-		<title>My homepage</title>
+		<meta charset="utf-8">
+		<title>Jade presents Trans-Coding</title>
+		<meta name="description" content="Jade presents Trans-Coding">
+		<meta name="author" content="Jade Selke">
+		<link rel="stylesheet" href="css/styles.css?v=1.0">
 	</head>
 	<body>
-		<h1>Web site tutorials</h1>
-		<br/>
-		<h2>Never more than 1 h1 tag</h2>
+		<script src="js/scripts.js"></script>
+		<h1>Modern Tutorials for Software/Website Development</h1>
+		<h2>A great place to learn to be a better coder, or just learn how to sell yourself!</h2>
 
-		<p>this is a <strong>paragraph</strong></p>
+		<p>My name is Jade, and I started a YouTube channel and this webpage to help you get a job in the tech world doing what you love. The best way to do that is to make a webpage that shows others what you can do. So join me, and together, we can really put your skills to best use in a way that will show a potential employer just what you are capable of!</p>
 		<hr/>
-		<!-- this is a note -->
-		<p>this is another <em>paragraph</em></p>
+
+		<p>If you would like to see my resume, <a href="resume.php">please click here</a>.</p>
+
+		<p>This site is a work in progress, and more in on the way soon.</p>
 	</body>
 </html>
---
+++ htdocs/resume.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+
+<style>
+	h1, h3{
+		text-align: center;
+	}
+	table{
+		width: 100%;
+		text-align: left;
+	}
+	.job-image{
+		width: 49%;
+	}
+</style>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Jade's Resume</title>
+		<meta name="description" content="Jade's Resume">
+		<meta name="author" content="Jade Selke">
+		<link rel="stylesheet" href="css/styles.css?v=1.0">
+	</head>
+	<body>
+
+		<!-- top of resume -->
+		<h1>JADE R. SELKE</h1>
+		<h3>
+			<a href="mailto:jade-transcoding@gmail.com?Subject=Re:Your%20Resume" target="_top">
+				jade-transcoding@gmail.com
+			</a>
+		</h3>
+		<hr/>
+		<!-- summary section -->
+		<p>SUMMARY</p>
+		<p>Team lead in software and website development, graduate of California State University, and veteran of the United States Air Force.</p>
+		<hr/>
+		<!-- skills section -->
+		<p>COMPUTER SKILLS</p>
+		<p>Languages: Proficient in: C++, C, JQuery, PHP, JavaScript, CSS/Bootstrap. Familiar with: Java, Visual Basic, XML, Swift, LISP.</p>
+		<p>Software: Database: MySQL Server, Microsoft Access, and Cloud SQL. Platforms: Windows, Linux (multiple distros), Android, and GitHub repository.</p>
+		<hr/>
+		<!-- experience section -->
+		<p>EXPERIENCE</p>
+		<table>
+			<tr>
+				<th>
+					Team Lead Full Stack (Lamp) Developer
+				</th>
+				<th>
+					<a href="https://www.magillaloans.com" target="_blank">Magilla Loans</a>
+				</th>
+				<th>
+					2018-present
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Increased user volume to main website by 25% over two month period by launching new SEM/SEO enhancements across 13 front facing landing pages.
+			</li>
+			<li>
+				Created comprehensive tool suite for users utilizing interface controls provided via API access to Google, Stripe, Experian, Zillow, Optimal Blue, and Black Knight.
+			</li>
+			<li>
+				Expanded impression and click through rates of ads by 20% by implementing real time monitoring of user interactions with AWS, Google Ad Manager and custom service components to maintain and analyze vendor interactions.
+			</li>
+			<li>
+				Reduced overall testing time of customer registration process by over 75% through a custom designed, Selenium based automated testing suite.
+			</li>
+			<li>
+				Language/Software used: PHP, CSS, JavaScript, JQuery, Java, Swift, MySQL, GitHub versioning
+			</li>
+		</ul>
+		<a href="https://www.magillaloans.com" target="_blank"><img class="job-image" src="mag.png"/></a>
+
+		<table>
+			<tr>
+				<th>
+					Senior Project
+				</th>
+				<th>
+					Catalysis Group, Inc.
+				</th>
+				<th>
+					Sacramento, CA	2017
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Authored new symmetric key cipher block and GitHub repository using SCRUM methodology.
+			</li>
+			<li>
+				Integrated automated testing suite and control flow graphs to verify code execution allowing for simplification of the software development cycle.
+			</li>
+			<li>
+				Coordinated tasks and communication with group of five senior level students.
+			</li>
+			<li>
+				Language/Software used: C, C++, Visual Studio, GitHub versioning
+			</li>
+		</ul>
+
+		<table>
+			<tr>
+				<th>
+					Web Dev/Computer Systems Analyst Intern
+				</th>
+				<th>
+					Black Diamond Data, Pittsburgh, CA
+				</th>
+				<th>
+					2014-2015
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Increased social awareness and activism efforts against cybercriminals and stalkers by maintaining Alliance Against Cybercrime website and social media presence.
+			</li>
+			<li>
+				Established a secure, city-wide Wi-Fi network for public use with interference collection techniques and testing industry standard security capabilities and protocols.
+			</li>
+			<li>
+				Coordinated interactions with San Francisco Electronic Crimes Task Force.
+			</li>
+			<li>
+				Language/Software used: HTML/PHP, CSS
+			</li>
+		</ul>
+		<div>
+			<a href="https://www.facebook.com/Black-Diamond-Data-170883673004075/" target="_blank"><img class="job-image" src="bdd.png"/></a>
+			<a href="https://www.facebook.com/allianceagainstcybercrime/" target="_blank"><img class="job-image" src="aac.png"/></a>
+		</div>
+		
+
+		<table>
+			<tr>
+				<th>
+					Airborne Cryptologic Linguist and Head of Security
+				</th>
+				<th>
+					United States Air Force
+				</th>
+				<th>
+					2003-2010
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Unified the physical, personnel, industrial, and information security standards while interacting with six national government intelligence agencies.
+			</li>
+			<li>
+				Maintained on call support 24/7 for 18 months by simultaneously performing the duties of three security manager personnel of higher rank.
+			</li>
+			<li>Software used: Microsoft Access</li>
+		</ul>
+
+		<hr/>
+		<!-- education section -->
+		<p>EDUCATION</p>
+		<table>
+			<tr>
+				<th>
+					California State University
+				</th>
+				<th>
+					Sacramento, CA
+				</th>
+				<th>
+					2015-2017
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Bachelor of Science in Computer Science, GPA in major: 3.75
+			</li>
+			<li>
+				Minor in Mathematics
+			</li>
+			<li>
+				Coursework: Computer System Attacks and Countermeasures, Cryptography, Intelligent Systems, Linear Programming, and Probability Theory
+			</li>
+		</ul>
+
+		<table>
+			<tr>
+				<th>
+					Contra Costa Community College
+				</th>
+				<th>
+					San Pablo, CA
+				</th>
+				<th>
+					2013-2015
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Major in Computer Science and Mathematics.
+			</li>
+		</ul>		
+
+		<table>
+			<tr>
+				<th>
+					Community College of the Air Force
+				</th>
+				<th>
+					Okinawa, Japan
+				</th>
+				<th>
+					2003-2010
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Obtained Certificate in Communications Technology and Records Custody.
+			</li>
+		</ul>		
+
+		<table>
+			<tr>
+				<th>
+					Defense Language Institute
+				</th>
+				<th>
+					Monterey, CA
+				</th>
+				<th>
+					2003-2005
+				</th>
+			</tr>
+		</table>
+		<ul>
+			<li>
+				Obtained Certificate for fluency in Mandarin Chinese.
+			</li>
+		</ul>		
+
+	</body>
+</html>
