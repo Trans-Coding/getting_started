37660f8d011ae25bbf6b264b757a4ac1980261a0
Jade <jade.transcoding@gmail.com>
2019-04-07 21:03:48 -0700

    episode 5 upload
    prepping for new storage system and uploads for episode 5, continuation of bootstrap/css styling

---
+++ htdocs/public_html/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Jade presents Trans-Coding</title>
+		<meta name="description" content="Jade presents Trans-Coding">
+		<meta name="author" content="Jade Selke">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="css/styles.css?v=1.0">
+		<link rel="apple-touch-icon" sizes="180x180" href="/public_html/pictures/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/public_html/pictures/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/public_html/pictures/favicon-16x16.png">
+		<link rel="manifest" href="/public_html/pictures/site.webmanifest">
+		<link rel="mask-icon" href="/public_html/pictures/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="shortcut icon" href="/public_html/pictures/favicon.ico">
+		<meta name="msapplication-TileColor" content="#b91d47">
+		<meta name="msapplication-config" content="/public_html/pictures/browserconfig.xml">
+		<meta name="theme-color" content="#941919">
+		<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+	</head>
+	<body>
+		<script src="js/scripts.js"></script>
+		<h1>Modern Tutorials for Software/Website Development</h1>
+		<h2>A great place to learn to be a better coder, or just learn how to sell yourself!</h2>
+
+		<p>My name is Jade, and I started a YouTube channel and this webpage to help you get a job in the tech world doing what you love. The best way to do that is to make a webpage that shows others what you can do. So join me, and together, we can really put your skills to best use in a way that will show a potential employer just what you are capable of!</p>
+		<hr/>
+
+		<p>If you would like to see my resume, <a href="resume.php">please click here</a>.</p>
+
+		<p>This site is a work in progress, and more in on the way soon.</p>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+</html>
