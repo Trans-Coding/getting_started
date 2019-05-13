915b674b664c915ff5c536b2df25a182c5afd56f
Jade <jade.transcoding@gmail.com>
2019-05-12 17:41:55 -0700

    update sql and readme
    Should now be ready to ship

--- htdocs/BHHC_application/Readme.txt
+++ htdocs/BHHC_application/Readme.txt
@@ -2,7 +2,7 @@ Jade Selke
 
 Brief Overview
 
-I extended this page from my site as if it were running on Laravel's MVC architecture. The main view is built in show.blade.php. The model and controller are both labeled as such, Application.php and ApplicationController.php. A calculator is also included to show usage of the Factory Method in the creation of a complex mortgage amortization table. For a demo of the page, simply load up the page as http://localhost/{location-of-file}/show.blade.php
+I extended this page from my site as if it were running on Laravel's MVC architecture. The main view is built in show.blade.php. The model and controller are both labeled as such, Application.php and ApplicationController.php. The .sql file that would be needed for creation of the table, insertion and selection is also provided. A calculator is included to show usage of the Factory Method in the creation of a complex mortgage amortization table. For a demo of the page, simply load up the page as http://localhost/{location-of-file}/show.blade.php
 
 Links Included
 My current employer: https://magillaloans.com
--- htdocs/BHHC_application/sqlCreation.sql
+++ htdocs/BHHC_application/sqlCreation.sql
@@ -9,4 +9,9 @@ CREATE TABLE application(
 );
 
 INSERT INTO applications(reason1, reason2, reason3)
-VALUES('<div>I have a great understanding of building relationships with customers. This comes from work at my current work, where we strive to create relationships between lenders and borrowers.</div>','<div>I want to learn from new challenges in a place that harbors growth from its employees. BHHC seems an ideal place to thrive, in a new environment that will provide challenges to help me grow as I create great applications that make a difference in the world.</div>','<div>I can make sophisticated calculators that run extremely fast on host machines! Example included and more advanced version found <a href="https://www.magillaloans.com/loans/calculators/affordability-calculator/">here</a>.</div>')
+VALUES('<div>I have a great understanding of building relationships with customers. This comes from work at my current work, where we strive to create relationships between lenders and borrowers.</div>','<div>I want to learn from new challenges in a place that harbors growth from its employees. BHHC seems an ideal place to thrive, in a new environment that will provide challenges to help me grow as I create great applications that make a difference in the world.</div>','<div>I can make sophisticated calculators that run extremely fast on host machines! Example included and more advanced version found <a href="https://www.magillaloans.com/loans/calculators/affordability-calculator/">here</a>.</div>');
+
+
+--method for pulling from the database
+SELECT * FROM `application` WHERE id = ?;
+--in this case, the $id from the file would be used in searching and saving the returned table
